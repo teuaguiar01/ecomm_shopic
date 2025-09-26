5ba09dabbca0a389b5f3f5a6d34d2c0a860e7b68
teuaguiar01 <teuaguiar01@gmail.com>
2025-09-25 21:11:08 -0300

    feat: firebase config

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: "shopic-2794b.appspot.com",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MSG_SENDERID,
   appId: process.env.FIREBASE_APPID
 };
